6ce3c1f139c115c41f08de53e7206c9cb78d788d
Jamie Peake <jamie@hive.com.au>
2016-06-24 15:48:45 +1000

    add the object contract

---
+++ Source/Contract/Object.php
@@ -0,0 +1,18 @@
+<?php namespace Hive\Benchmark\Contract;
+
+    /**
+     * Object Interface.
+     *
+     * Object for the library.
+     *
+     * @author Jamie Peake <jamie.peake@gmail.com>
+     * @licence https://github.com/hive/benchmark/blob/master/LICENSE (BSD-3-Clause)
+     *
+     * @package Hive
+     * @subpackage Benchmark
+     *
+     * @copyright (c) 2016 Jamie Peake
+     */
+    interface Object
+    {
+    }
