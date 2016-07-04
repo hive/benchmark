849c140222027df8351f09f11ff4df9d5ca7b185
Jamie Peake <jamie@hive.com.au>
2016-07-05 01:25:33 +1000

    scaffold tests, add ObjectGet

--- Tests/testInstanceGet.php
+++ Tests/testInstanceGet.php
@@ -1,7 +1,9 @@
 <?php
+    
 class TestInstanceGet extends PHPUnit_Framework_TestCase
 {
-    public function testSanity() {
-        $this->assertEquals(1+1,2);
+    public function testSanity()
+    {
+        $this->assertEquals(1+1, 2);
     }
 }
--- Tests/testInstanceSummary.php
+++ Tests/testInstanceSummary.php
@@ -1,7 +1,9 @@
 <?php
+    
 class TestInstanceSummary extends PHPUnit_Framework_TestCase
 {
-    public function testSanity() {
-        $this->assertEquals(1+1,2);
+    public function testSanity()
+    {
+        $this->assertEquals(1+1, 2);
     }
 }
