431b5bdcac043783b5688f7769539c04f1afef03
Jamie Peake <jamie@hive.com.au>
2016-07-13 11:32:30 +1000

    Update Documentation

--- Build/run.sh
+++ Build/run.sh
@@ -12,14 +12,18 @@ phpmd ../Source xml codesize,controversial,design,naming,unusedcode --reportfile
 # Output the rules of the CodeSniffer
 phpcs --generator=html > ../Documents/html/Sniffer/rules.html;
 
-# Run php Cod Sniffer
-phpcs ../Source/
+# Run php Code Sniffer
+phpcs ../Source/;
 
 # Run it a second time gathering all the output into one file
 phpcs --standard=PSR1,PSR2  --report-info --report-full --report-source --report-summary ../Source > ../Documents/html/sniffer/all.txt  > ../Documents/html/sniffer/all.txt;
 
 # Create phpUnit html
-xsltproc templates/phpunit.xlst ../Documents/xml/phpunit.xml > ../Documents/html/phpunit/index.html
+xsltproc templates/phpunit.xls ../Documents/xml/phpunit.xml > ../Documents/html/phpunit/index.html;
+
+# Create codeSniffer html
+xsltproc templates/codesniffer.xls ../Documents/xml/codesniffer.xml > ../Documents/html/codesniffer/index.html;
+
 
 # Run phpDox
 ~/Sites/phpdox/phpdox -f phpdox.xml;
