355086fa8e16ae9bb810985d2e3b731811fdd0c1
Jamie Peake <jamie@hive.com.au>
2016-07-13 12:38:42 +1000

    Update Documentation

--- Build/run.sh
+++ Build/run.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-phpcs ../Source/;
-
 
 # Gather php lines of code
 phploc --log-xml ../Documents/xml/phploc.xml --progress ../Source/;
@@ -9,23 +7,19 @@ phploc --log-xml ../Documents/xml/phploc.xml --progress ../Source/;
 # Run phpunit
 phpunit --configuration phpunit.xml --testsuite All;
 
-
-
-
 # Run php mess detector
 phpmd ../Source xml codesize,controversial,design,naming,unusedcode --reportfile ../Documents/xml/phpmd.xml --suffixes php;
+
+# Run php Code Sniffer
+phpcs --tab-width=auto  --standard=PSR1,PSR2  --report-info=../Documents/html/Sniffer/info.txt --report-full=../Documents/html/Sniffer/full.txt --report-source=../Documents/html/Sniffer/source.txt --report-summary=../Documents/html/Sniffer/summary.txt --report-xml=../Documents/xml/codesniffer.xml ../Source/;
+
 
 # Output the rules of the CodeSniffer
 #phpcs --generator=html > ../Documents/html/Sniffer/rules.html;
 
-# Run php Code Sniffer
-
 
 # Run it a second time gathering all the output into one file
-#phpcs --standard=PSR1,PSR2  --report-info --report-full --report-source --report-summary ../Source > ../Documents/html/sniffer/all.txt  > ../Documents/html/sniffer/all.txt;
-
-#they need to all be ran at once
-phpcs --generator=html > ../Documents/html/Sniffer/rules.html;phpcs --standard=PSR1,PSR2  --report-info --report-full --report-source --report-summary ../Source > ../Documents/html/sniffer/all.txt  > ../Documents/html/sniffer/all.txt;
+phpcs --standard=PSR1,PSR2 --tab-width=auto --report-info --report-full --report-source --report-summary ../Source > ../Documents/html/sniffer/all.txt  > ../Documents/html/sniffer/all.txt;
 
 # Create phpUnit html
 xsltproc templates/phpunit.xls ../Documents/xml/phpunit.xml > ../Documents/html/phpunit/index.html;
