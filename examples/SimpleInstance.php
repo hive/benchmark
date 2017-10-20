a753840f0192fc3a0658782fd8c87514391e9d28
Jamie Peake <jamie@hive.com.au>
2017-10-20 15:31:53 +1100

    clean up instance example

--- examples/SimpleInstance.php
+++ examples/SimpleInstance.php
@@ -7,46 +7,9 @@
 // Include the package, using the simple php include
 include __DIR__ . '/../include.php';
 
-// Create the object
-$bm = new Hive\Benchmark\Object();
+// Use the usage.
+use \Hive\Benchmark;
 
-// Start the mark
-$bm->start('NameOfMark');
-
-// Stop the mark
-$bm->stop('NameOfMark');
-
-// Output a summary
-var_dump($bm->summary());
-die();
-
-var_dump($result);
-
-die();
-Hive\Benchmark\Instance::config(['enabled' => false]);
-Hive\Benchmark\Instance::start('test');
-Hive\Benchmark\Instance::stop('test');
-
-Hive\Benchmark\Instance::config(['enabled' => true]);
-Hive\Benchmark\Instance::start('test2');
-Hive\Benchmark\Instance::stop('test2');
-
-var_dump(Hive\Benchmark\Instance::summary());
-die();
-// Use the benchmark
-use Hive\Benchmark;
-
-$bm = new \Hive\Benchmark\Object(['enabled' => false]);
-$bm->start('test');
-$bm->stop('test');
-$bm->start('test');
-$bm->stop('test');
-$result = $bm->details('test');
-var_dump($result);
-die();
-print_r(Benchmark\Instance::get('NameOfBenchmark'));
-
-die();
 // Start the benchmark
 Benchmark\Instance::start('NameOfBenchmark');
 
