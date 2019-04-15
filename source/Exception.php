4b020d046fc021a5488752b8f707132a146c7700
Jamie Peake <jamie@hive.com.au>
2019-04-15 18:24:22 +1000

    update package number

--- source/Exception.php
+++ source/Exception.php
@@ -25,7 +25,7 @@ class Exception extends \Exception
     /**
      * Package exception code, sequential package numbers.
      */
-    const PACKAGE_NUMBER = 1;
+    const PACKAGE_NUMBER = 10;
 
     /**
      * Exception constructor, assigns the message and code and calls the exception handler.
